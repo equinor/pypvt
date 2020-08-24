3f14986757711f8ba102dc3c81ab9267718eca0f
Eivind Smoergrav <eism@statoil.com>
2020-08-24 10:35:53 +0200

    Start using the BoPVT check routines

--- pypvt/pvt_consistency_check.py
+++ pypvt/pvt_consistency_check.py
@@ -8,11 +8,15 @@ def main(case_name):
     fluid_description = FieldFluidDescription(ecl_case=case_name)
 
     print(fluid_description.validate_description())
-    # print (fluid_description.fluid_descriptions[0].rvvd_rv)
-    # print (fluid_description.fluid_descriptions[0].rvvd_depth)
+
     # loop over all equil regions
     # check consistency in pvt
+    for fluid in fluid_description.fluid_descriptions:
 
+        fluid.calc_fluid_prop_vs_depth(no_nodes=4)
+        fluid.print_depth_tables()
+        fluid.inplace_report()
+        fluid.pvt_gradient_check()
 
 if __name__ == "__main__":
     main(sys.argv[1])
