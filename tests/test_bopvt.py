35ee86b0ec325a97a4fc589ae91a099851d8fdbe
Eivind Smoergrav <eism@statoil.com>
2020-08-24 12:55:30 +0200

    Add testmodule for bopvt, ampthy as of now

---
+++ tests/test_bopvt.py
@@ -0,0 +1,19 @@
+"""Test bopvt"""
+
+import pathlib
+
+import numpy as np
+import pandas as pd
+import ecl2df
+
+from pypvt import BoPVT
+
+TESTDATA = pathlib.Path(__file__).resolve().parent / "data"
+
+
+def test_main():
+    pass
+
+
+if __name__ == "__main__":
+    test_main()
